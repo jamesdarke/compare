09f1ee12750e79b8c877ad34e4b6c99c19044430
jamesdarke <98935542+jamesdarke@users.noreply.github.com>
2022-04-06 13:40:42 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-@run_async
-def start(update, context):
     start_string = f'''
 This bot can mirror all your links to Google drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
