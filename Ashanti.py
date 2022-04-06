2ca5cc0487c1e619f255b1e4d3edb88808efba9d
jamesdarke <98935542+jamesdarke@users.noreply.github.com>
2022-04-06 12:54:00 +0530

    Update Ashanti.py

--- Ashanti.py
+++ Ashanti.py
@@ -68,6 +68,7 @@ This bot can mirror all your links to Google drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN)
+    
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
